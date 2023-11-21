8b451b3aa1ad8361b38e89cba5b418a53d41818e
Fayoz <qahramonovfayozfcb@gmail.com>
2023-11-21 10:50:01 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is Header Content
       </header>
+      <main>
+        This is Main Content
+      </main>
+      <footer>
+        This is Footer Content
+      </footer>
     </div>
   );
 }
